c7dc571288c40a9583a2acaa5fd864ce9c7af12d
“Kakaroco88” <aswadh250@gmail.com>
2025-01-10 09:45:07 +0700

    komit tgl 10

--- src/config.ts
+++ src/config.ts
@@ -34,17 +34,17 @@ export const SITE = {
 	image:
 		"https://raw.githubusercontent.com/one-aalam/astro-ink/main/public/astro-banner.png",
 	// YT video channel Id (used in media.astro)
-	ytChannelId: "",
+	ytChannelId: "@kakaroco88",
 	// Optional, user/author settings (example)
 	// Author: name
-	author: "", // Example: Fred K. Schott
+	author: "kakaroco88", // Example: Fred K. Schott
 	// Author: Twitter handler
-	authorTwitter: "", // Example: FredKSchott
+	authorTwitter: "@kakaroco88", // Example: FredKSchott
 	// Author: Image external source
 	authorImage: "", // Example: https://pbs.twimg.com/profile_images/1272979356529221632/sxvncugt_400x400.jpg, https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png
 	// Author: Bio
 	authorBio:
-		"AuthorBio : Namaku Kakaroco88 berjalan setiap hari dan datanglah inspirasi",
+		"Jangan takut untuk tersesat, karena di situlah petualangan sesungguhnya dimulai. Kakaroco88 akan membantumu menemukan jalan.",
 };
 
 // Ink - Theme configuration
